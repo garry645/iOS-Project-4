2d1a8a92cc8619fb7f821f7aa45058721f7f8740
Garrison <garry645.gh@gmail.com>
2020-06-21 20:37:36 -0500

    Initial Commit

---
+++ ProgrammingProject2a-GarrisonHatcher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProgrammingProject2a-GarrisonHatcher
+//
+//  Created by Garrison Hatcher on 6/21/20.
+//  Copyright © 2020 Garrison Hatcher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
